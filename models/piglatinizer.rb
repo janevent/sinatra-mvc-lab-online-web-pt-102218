352e9dff3191c11a4a5c62351ed204b114054055
Jane Venter <janeanneventer@gmail.com>
2019-02-25 05:49:20 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class PigLatinizer 
+  
+  attr_reader :phrase
+  
+  # def splits
+  #   phrase.split(" ")
+  # end
+  
+  def piglatinize(phrase)
+    array = phrase.split(" ")
+    array.map do |word|
+      letters = word.split("")
+      #binding.pry
+      if !letters.first.scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !letters[1].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? 
+        #binding.pry
+        c = letters.shift 
+        letters << c
+        d = letters.shift 
+        letters << d
+        letters << "ay"
+        letters.join
+      elsif !letters.first.scan(/[bcdfghjklmnpqrstvwxyz]/).empty? 
+        c = letters.shift 
+        letters << c
+        letters << "ay"
+        letters.join
+      else
+        letters << "way"
+        letters.join
+      end
+    end.join(" ")
+  end
+  
+end
